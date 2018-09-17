8a357bf9c8ae9fdc8587ee2887faec3abf65b66b
taesch124 <taesch124@gmail.com>
2018-09-16 20:25:13 -0400

    Created responsive portfolio page using media queries and added javascript to header so that it shrinks on scroll.

---
+++ week-02/homework-02/responsive-portfolio/assets/scripts/header.js
@@ -0,0 +1,25 @@
+const wrapper = document.getElementById('wrapper');
+const header = document.querySelector('header');
+const headerContent = document.querySelector('.header_content');
+const mainContent = document.querySelector('.main_content');
+const lastName = document.getElementById('header_last_name');
+const nameText = document.getElementById('name_text');
+
+function shrinkName() {
+    const distY = wrapper.scrollTop - header.offsetHeight;
+
+    if (distY > 0) {
+        lastName.classList.remove('new_line');
+        nameText.classList.add('smaller_header');
+        headerContent.style.paddingBottom = 0;
+    } else {
+        lastName.classList.add('new_line');
+        nameText.classList.remove('smaller_header');
+        mainContent.scrollTop = header.offsetHeight;
+        headerContent.style.paddingBottom = ".75em";
+    }
+
+}
+
+window.addEventListener
+wrapper.addEventListener('scroll', function() {shrinkName();});
