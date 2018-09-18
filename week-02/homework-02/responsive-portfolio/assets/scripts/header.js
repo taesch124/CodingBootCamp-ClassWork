b72810e3bec70cf24af336b7d16889107406945a
taesch124 <taesch124@gmail.com>
2018-09-17 22:45:26 -0400

    Changed JavaScript so shrinking header on scroll also reduces header box shadow.

--- week-02/homework-02/responsive-portfolio/assets/scripts/header.js
+++ week-02/homework-02/responsive-portfolio/assets/scripts/header.js
@@ -12,11 +12,13 @@ function shrinkName() {
         lastName.classList.remove('new_line');
         nameText.classList.add('smaller_header');
         headerContent.style.paddingBottom = 0;
+        header.style.boxShadow = "0 1px 1px #474545";
     } else {
         lastName.classList.add('new_line');
         nameText.classList.remove('smaller_header');
         mainContent.scrollTop = header.offsetHeight;
         headerContent.style.paddingBottom = ".75em";
+        header.style.boxShadow = "0 3px 3px #474545";
     }
 
 }
